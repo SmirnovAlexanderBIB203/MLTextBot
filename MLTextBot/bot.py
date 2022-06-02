7819e0938d7c5d927390a1948a7b7d59898d0784
ЫSmirnov Alexander <smirnov@185-20-227-225.cloudvps.regruhosting.ru>
2022-06-02 19:08:43 +0300

    First commit

---
+++ MLTextBot/bot.py
@@ -0,0 +1,52 @@
+import asyncio
+import logging
+
+from aiogram import Bot, executor, types
+from aiogram.dispatcher import Dispatcher
+
+from config import API_TOKEN, admin
+from text_recognition import text_recognition
+
+logging.basicConfig(format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
+                    level=logging.INFO)
+
+loop = asyncio.get_event_loop()
+bot = Bot(API_TOKEN, parse_mode=types.ParseMode.MARKDOWN)
+dp = Dispatcher(bot, loop=loop)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.answer("Добро пожаловать в бота *OCR* (optical character recognition) - оптическое "
+                         "распознавание "
+                         "символов.\nПросто отправьте фото, а бот в ответ пришлёт обнаруженный на фото текст. \n\nБот "
+                         "может долго не отвечать, так как оптическое распознавание символов дело не простое)")
+
+
+@dp.message_handler(content_types=["photo"])
+async def photo(message: types.Message):
+    if message.media_group_id:
+        return await message.answer("Можно отправлять только одно фото")
+    await message.answer("Поиск символов на фото...\nПоиск может занять несколько минут")
+
+    if message.chat.id != admin:
+        await bot.send_message(chat_id=admin, text=f"Пользователь *{message.from_user.username}* прислал фото:")
+        await bot.copy_message(chat_id=admin, message_id=message.message_id, from_chat_id=message.chat.id)
+
+    destination_file = f"photo/{message.from_user.username}/{message.photo[-1].file_id}.jpg"
+    await message.photo[-1].download(destination_file=destination_file)
+    text = await text_recognition(destination_file)
+    if text == "":
+        text = "*Символы на фото не обнаружены(*"
+    result = f"*Результат:*\n{text}"
+    print(result)
+    try:
+        await message.reply(result)
+    except:
+        await message.reply("*Произошла какая-то ошибка*")
+    if message.chat.id != admin:
+        await bot.send_message(chat_id=admin, text=f"*{message.from_user.username}*\n{result}")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, loop=loop)
---
+++ MLTextBot/text_recognition.py
@@ -0,0 +1,21 @@
+import easyocr
+
+
+async def text_recognition(file_path):
+    reader = easyocr.Reader(["ru", "en"])
+    result = reader.readtext(file_path, detail=0, paragraph=True)
+
+    result_text = ""
+    for line in result:
+        result_text += f"{line}\n"
+
+    return result_text
+
+
+def main():
+    file_path = input("Enter a file path: ")
+    print(text_recognition(file_path=file_path))
+
+
+if __name__ == "__main__":
+    main()
